62debb1558653b72ede948db6a623dd1dfc88e7e
Fabian Kędzierski <grafska@gmail.com>
2016-06-02 23:29:23 +0200

    Change to Rspec tests

--- lib/rails_voter/engine.rb
+++ lib/rails_voter/engine.rb
@@ -1,5 +1,12 @@
 module RailsVoter
   class Engine < ::Rails::Engine
     isolate_namespace RailsVoter
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.assets true
+      g.helper false
+    end
   end
 end
--- test/rails_voter_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RailsVoterTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, RailsVoter
-  end
-end
