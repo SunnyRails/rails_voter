9407cbf95a209efee73d0300e326b3e2a77eec79
Fabian Kędzierski <grafska@gmail.com>
2016-06-09 18:33:03 +0200

    Remove migrations in spec/dummy/db

--- spec/dummy/db/migrate/20160609160014_add_ref_to_ballot_form_details.rails_voter.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from rails_voter (originally 20160607180028)
-class AddRefToBallotFormDetails < ActiveRecord::Migration
-  def change
-    add_reference :rails_voter_ballot_form_details, :ballot_form, index: true, foreign_key: true
-  end
-end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609160016) do
+ActiveRecord::Schema.define(version: 20160608145149) do
 
   create_table "rails_voter_ballot_form_details", force: :cascade do |t|
     t.string   "name"
