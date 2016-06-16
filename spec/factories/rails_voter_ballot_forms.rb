856e1f71670c62cb254447c640a842461fa821e5
Fabian Kędzierski <grafska@gmail.com>
2016-06-16 16:08:36 +0200

    Refactor code

--- spec/factories/rails_voter_ballot_forms.rb
+++ spec/factories/rails_voter_ballot_forms.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :rails_voter_ballot_form, class: 'RailsVoter::BallotForm' do
-    name 'BallotForm - name'
+  factory :rails_voter_ballot_form, class: "RailsVoter::BallotForm" do
+    name "BallotForm - name"
   end
 end
