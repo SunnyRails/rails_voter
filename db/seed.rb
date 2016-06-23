1be4e28e9cc54f8360ea2b410fcae0346689a9d2
rafpiek <rafpiek@gmail.com>
2016-06-23 18:15:14 +0200

    Solve bug in seeds.

--- db/seed.rb
+++ db/seed.rb
@@ -2,17 +2,12 @@ separator = ">>>>>>>><<<<<<<<<<"
 
 p separator
 
-  ballot_form_id = 0
-  ballot_form_detail_id = 0
-
 5.times do
-  ballot_form_id += 1
-  RailsVoter::BallotForm.create(name:Faker::StarWars.planet)
+  bf = RailsVoter::BallotForm.create(name:Faker::StarWars.planet)
     2.times do
-      ballot_form_detail_id += 1
-      RailsVoter::BallotFormDetail.create(name:Faker::StarWars.character,ballot_form_id:ballot_form_id)
+      bfd = bf.ballot_form_details.create(name:Faker::StarWars.character)
         3.times do
-          RailsVoter::VotingResult.create(ballot_form_detail_id: ballot_form_detail_id)
+          bfd.voting_results.create
         end
     end
 end
