794275b0ee681edf1f887145452051405b8c7946
rafpiek <rafpiek@gmail.com>
2016-06-24 16:17:44 +0200

    Correct ruby syntax in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@ p separator
 
 5.times do
   bf = RailsVoter::BallotForm.create(name: Faker::StarWars.planet)
-    2.times do
-      bfd = bf.ballot_form_details.create(name: Faker::StarWars.character)
-        3.times do
-          bfd.voting_results.create
-        end
+  2.times do
+    bfd = bf.ballot_form_details.create(name: Faker::StarWars.character)
+    3.times do
+      bfd.voting_results.create
     end
+  end
 end
 
 message = "5 ballot forms, 10 ballot form details and 30 voting results created."
