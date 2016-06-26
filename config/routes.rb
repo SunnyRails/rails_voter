47511953062b8b2601cc3a503b297031aa73af23
Fabian Kędzierski <grafska@gmail.com>
2016-06-26 19:19:06 +0200

    Add vote result page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RailsVoter::Engine.routes.draw do
   resources :ballot_forms do
     get "vote", on: :member
+    get "show_result", on: :member
   end
 
   resources :voting_results, only: [:create]
