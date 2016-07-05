303184a48fe5fa64b021cbdbcaca91ca6ed13444
Fabian Kędzierski <grafska@gmail.com>
2016-07-05 21:05:02 +0200

    Add USA presidential election seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 require "faker"
 
+p "Remove seeds"
+RailsVoter::VotingResult.destroy_all
+RailsVoter::BallotFormDetail.destroy_all
+RailsVoter::BallotForm.destroy_all
+
 separator = ">>>>>>>><<<<<<<<<<"
 
 p separator
@@ -18,3 +23,19 @@ message = "5 ballot forms, 10 ballot form details and 30 voting results created.
 p separator
 p message
 p separator
+
+p "United States presidential election, 2016 seeds"
+
+bf = RailsVoter::BallotForm.create(name: "United States presidential election, 2016")
+
+bfd = bf.ballot_form_details.create(name: "Hillary Clinton")
+4.times { bfd.voting_results.create }
+
+bfd = bf.ballot_form_details.create(name: "Donald Trump")
+3.times { bfd.voting_results.create }
+
+bfd = bf.ballot_form_details.create(name: "Gary Johnson")
+2.times { bfd.voting_results.create }
+
+bfd = bf.ballot_form_details.create(name: "Jill Stein")
+2.times { bfd.voting_results.create }
