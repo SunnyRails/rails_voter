4997e0f93bb3a1037b1b14287f5940e8cd2521eb
Fabian Kędzierski <grafska@gmail.com>
2016-07-07 19:44:25 +0200

    Fix ruby syntax

--- spec/models/rails_voter/election_detail_spec.rb
+++ spec/models/rails_voter/election_detail_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 module RailsVoter
   RSpec.describe ElectionDetail, type: :model do
-    describe 'database columns' do
+    describe "database columns" do
       it { should have_db_column :name }
       it { should have_db_column :election_id }
     end
 
-    describe 'associations' do
+    describe "associations" do
       it { is_expected.to belong_to :election }
       it { is_expected.to have_many :voting_results }
     end  end
--- spec/models/rails_voter/election_spec.rb
+++ spec/models/rails_voter/election_spec.rb
@@ -2,19 +2,19 @@ require "rails_helper"
 
 module RailsVoter
   RSpec.describe Election, type: :model do
-    describe 'validations' do
+    describe "validations" do
       it { is_expected.to validate_presence_of :name }
     end
 
-    describe 'nested attributes' do
+    describe "nested attributes" do
       it { should accept_nested_attributes_for :election_details }
     end
 
-    describe 'datatabase columns' do
+    describe "datatabase columns" do
       it { should have_db_column :name }
     end
 
-    describe 'assocations' do
+    describe "assocations" do
       it { is_expected.to have_many :election_details }
     end
   end
