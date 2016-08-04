8f98b185fb6410cf105eef6cd96fce3d64264a52
Fabian Kędzierski <grafska@gmail.com>
2016-08-04 15:43:37 +0200

    Remove migration in dummy app

--- spec/dummy/db/migrate/20160804125929_create_rails_voter_voting_results.rails_voter.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from rails_voter (originally 20160608141052)
-class CreateRailsVoterVotingResults < ActiveRecord::Migration
-  def change
-    create_table :rails_voter_voting_results do |t|
-      t.timestamps null: false
-    end
-  end
-end
--- spec/dummy/db/migrate/20160804125930_add_ref_to_rails_voter_voting_result.rails_voter.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from rails_voter (originally 20160608145149)
-class AddRefToRailsVoterVotingResult < ActiveRecord::Migration
-  def change
-    add_reference :rails_voter_voting_results, :ballot_form_detail, index: true, foreign_key: true
-  end
-end
