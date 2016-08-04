2e2347e5d2583b6e31c9ad00842124addb3e2016
Fabian Kędzierski <grafska@gmail.com>
2016-08-04 18:26:21 +0200

    Add 'voting result' helper (#75)

--- app/helpers/rails_voter/application_helper.rb
+++ app/helpers/rails_voter/application_helper.rb
@@ -1,11 +1,21 @@
 module RailsVoter
   module ApplicationHelper
     def embed_ballot_form(id)
-      content_tag(:div, nil, class: "embed-container") do
+      content_tag(:div, nil) do
         content_tag(
           :iframe,
           nil,
           src: rails_voter.vote_election_path(id),
+        )
+      end
+    end
+
+    def embed_voting_result(id)
+      content_tag(:div, nil) do
+        content_tag(
+          :iframe,
+          nil,
+          src: rails_voter.show_result_election_path(id),
         )
       end
     end
